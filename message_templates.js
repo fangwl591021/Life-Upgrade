95f736c0e67997c32f99b94573d61bde1bf6073c
fangwl591021 <50135766+fangwl591021@users.noreply.github.com>
2026-04-11 22:32:30 +0800

    Update message_templates.js

--- message_templates.js
+++ message_templates.js
@@ -17,28 +17,22 @@ export function generateOrderListFlexMessage(orders) {
       { type: "button", action: { type: "message", label: "取消報名", text: "我想取消報名 (單號:" + order.orderId + ")" }, style: "secondary", height: "md" }
     ]}
   }));
-  return { type: "flex", altText: "您的報名紀錄", contents: { type: "carousel", contents: bubbles } };
+  return { type: "flex", altText: "您的預約報名紀錄", contents: { type: "carousel", contents: bubbles } };
 }
 
 export function generateCategoryFlexMessage(categories) {
   if (!categories || categories.length === 0) return null;
-  const categoryImages = {
-    "蛻變階段": "https://s3.us-west-1.wasabisys.com/aitw/2026/04/b8721597914eb3e6352ad3c30e68b153.jpg",
-    "完整階段": "https://s3.us-west-1.wasabisys.com/aitw/2026/04/dd11e3a570c5fccccc5fee72f639cfda.jpg",
-    "一般課程": "https://s3.us-west-1.wasabisys.com/aitw/2026/04/25b2916b5c49db617f52fa5ea48efee7.jpg",
-    "工作坊課程": "https://s3.us-west-1.wasabisys.com/aitw/2026/04/c4ca4238a0b923820dcc509a6f75849b.png"
-  };
   const bubbles = categories.map(category => ({
     type: "bubble", size: "micro",
     body: { type: "box", layout: "vertical", paddingAll: "0px", contents: [
-      { type: "image", url: categoryImages[category] || "https://s3.us-west-1.wasabisys.com/aitw/2026/04/c81e728d9d4c2f636f067f89cc14862c.png", size: "full", aspectRatio: "20:13", aspectMode: "cover" },
+      { type: "image", url: "https://s3.us-west-1.wasabisys.com/aitw/2026/04/c81e728d9d4c2f636f067f89cc14862c.png", size: "full", aspectRatio: "20:13", aspectMode: "cover" },
       { type: "box", layout: "vertical", paddingAll: "sm", contents: [{ type: "text", text: category, weight: "bold", size: "md", align: "center", color: "#000000" }]}
     ]},
     footer: { type: "box", layout: "vertical", paddingAll: "xs", contents: [
       { type: "button", action: { type: "message", label: "查看課程", text: "我想查詢 " + category + " 的課程" }, style: "primary", height: "sm", color: "#007AFF" }
     ]}
   }));
-  return { type: "flex", altText: "請選擇課程類型", contents: { type: "carousel", contents: bubbles } };
+  return { type: "flex", altText: "請選擇課程類別", contents: { type: "carousel", contents: bubbles } };
 }
 
 export function generateCourseFlexMessage(courses) {
@@ -56,10 +50,10 @@ export function generateCourseFlexMessage(courses) {
     footer: { type: "box", layout: "vertical", spacing: "md", contents: [
       { type: "separator" },
       { type: "box", layout: "horizontal", spacing: "md", contents: [
-        { type: "button", action: { type: "uri", label: "課程說明 >", uri: "https://liff.line.me/2009130603-ktCTGk6d?id=" + course.id }, style: "link", height: "sm" },
+        { type: "button", action: { type: "uri", label: "說明 >", uri: "https://liff.line.me/2009130603-ktCTGk6d?id=" + course.id }, style: "link", height: "sm" },
         { type: "button", action: { type: "message", label: "我要報名", text: "我想預約 " + course.name + " (編號:" + course.id + ", 金額:" + course.price + ")" }, style: "primary", height: "md", color: "#007AFF" }
       ]}
     ]}
   }));
-  return { type: "flex", altText: "為您挑選的課程", contents: { type: "carousel", contents: bubbles } };
+  return { type: "flex", altText: "人生進化 Action 精選課程", contents: { type: "carousel", contents: bubbles } };
 }
